6a2b2bbf744ad821b445292d767baf16d4e286e2
Simon Castellanos <simoncastellanos9@gmail.com>
2021-07-31 16:17:20 -0500

    populated dropdown and select new sample.

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,27 @@
-const bbBioData = "samples.json";
+function init(){
+    var selector = d3.select("#selDataset");
 
-d3.json(bbBioData).then(function(data){
-  console.log(data);
-});
+    d3.json("samples.json").then((data) =>{
+      let testData = data;
+        var subjectName = data.names;
+        subjectName.forEach((ID) => {
+            selector.append('option').text(ID).property('value', ID);
+        });
+    var initSample = subjectName[0];
+    test(initSample);
+    //updateCharts(firstbutton);
+    //updateMetadata(firstbutton);
+    });
+};
+
+function optionChanged(newSample) {
+  test(newSample);
+  //updateMetadata(newSample);
+  //updateCharts(newSample);
+}
+
+function test(button){
+  console.log(button);
+};
+
+init();
