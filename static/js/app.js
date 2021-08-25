7153f9585ad887cf02dc47a0a3362a18e1014706
Simon Castellanos <simoncastellanos9@gmail.com>
2021-08-25 16:06:11 -0500

    Comments in app and bonus files

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,18 @@
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
-    // Filter the data for the object with the desired sample number
+    // Filter the data
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
-    // Use d3 to select the panel with id of `#sample-metadata`
+    // Select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
 
-    // Use `.html("") to clear any existing metadata
     PANEL.html("");
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
     Object.entries(result).forEach(([key, value]) => {
       PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
     });
 
-    // BONUS: Build the Gauge Chart
     buildGauge(result.wfreq);
   });
 }
@@ -32,7 +27,7 @@ function buildCharts(sample) {
     var otu_labels = result.otu_labels;
     var sample_values = result.sample_values;
 
-    // Build a Bubble Chart
+    //Bubble Chart
     var bubbleLayout = {
       title: "Bacteria Cultures Per Sample",
       margin: { t: 0 },
@@ -77,10 +72,8 @@ function buildCharts(sample) {
 }
 
 function init() {
-  // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
-  // Use the list of sample names to populate the select options
+  //Populate dropdown
   d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
 
@@ -91,7 +84,7 @@ function init() {
         .property("value", sample);
     });
 
-    // Use the first sample from the list to build the initial plots
+    // build the initial plots
     var firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
@@ -99,7 +92,7 @@ function init() {
 }
 
 function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
+  // fetch new data
   buildCharts(newSample);
   buildMetadata(newSample);
 }
--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,18 +1,13 @@
-/**
- * BONUS Solution
- * */
 function buildGauge(wfreq) {
-  // Enter the washing frequency between 0 and 180
+
   var level = parseFloat(wfreq) * 20;
 
-  // Trig to calc meter point
   var degrees = 180 - level;
   var radius = 0.5;
   var radians = (degrees * Math.PI) / 180;
   var x = radius * Math.cos(radians);
   var y = radius * Math.sin(radians);
 
-  // Path: may have to change to create a better triangle
   var mainPath = "M -.0 -0.05 L .0 0.05 L ";
   var pathX = String(x);
   var space = " ";
